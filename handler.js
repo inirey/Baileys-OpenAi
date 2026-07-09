bff7442b073571d2c21c65fe8cd9f00f7daeb7e3
Rey <gansrey97@gmail.com>
2026-07-10 00:53:39 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,1 +1,15 @@
-ok
+module.exports = async (sock, msg) => {
+
+    const text =
+        msg.message?.conversation ||
+        msg.message?.extendedTextMessage?.text ||
+        ""
+
+    if (!text) return
+
+    console.log("Pesan :", text)
+
+    await sock.sendMessage(msg.key.remoteJid, {
+        text: `Kamu berkata:\n${text}`
+    })
+}
